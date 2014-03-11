eea216a25726109cfb4501e431430c2116eb4c22
Sabina <bunnimina@gmail.com>
2014-03-11 09:28:48 -0400

    Upload index, shopping list javascript, and css

---
+++ shoppinglist.js
@@ -0,0 +1,18 @@
+
+var index=0;
+var shoppingList =[];
+
+$("#submit").click(function(){
+      //log input value
+      var items= $("#inputItem").val();
+      console.log(items);
+  
+     //log list of inputs
+      shoppingList.push(items);
+      console.log(shoppingList);
+    
+     
+     
+                
+});
+
