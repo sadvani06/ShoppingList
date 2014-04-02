a1f0acdd0d561eaa2f10a53a77925d8d395c6c65
Sabina <bunnimina@gmail.com>
2014-04-02 09:10:44 -0400

    updated index, css, and js files

--- shoppinglist.js
+++ shoppinglist.js
@@ -1,18 +1,37 @@
-
-var index=0;
-var shoppingList =[];
-
-$("#submit").click(function(){
-      //log input value
-      var items= $("#inputItem").val();
-      console.log(items);
-  
-     //log list of inputs
-      shoppingList.push(items);
-      console.log(shoppingList);
-    
-     
-     
-                
+$("#inputItem").keyup(function(event){
+    if(event.keyCode == 13){
+            $("#addBtn").click();
+    }
 });
+
+$("#addBtn").click(function(){
+  var newItem= $("#inputItem").val();
+ 
+       if (newItem===null || newItem==="")
+        {
+          alert("Do not leave the field blank");
+        }
+
+        else{
+         
+
+          var listItem= "<li><input type='checkbox' id='check'><label for='check'>" + newItem + "</label></li>";
+
+          $( "#list" ).append( listItem );
+          console.log(listItem);
+
+          $("#inputItem").val('');
+        }
+});
+
+$("#removeBtn").click(function(){
+ 
+ $.each( $("li").has( "input[type='checkbox']:checked" ), function(){
+           /* var i = listArray.indexOf( this );
+            listArray.splice( i, 1 );*/
+   $(this).remove();
+	            
+  });
+        
+  });
 
